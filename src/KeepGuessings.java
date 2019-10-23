3296693f74e9f1bc1185e1c55cef60237a8a1ea6
hayle778 <hayjclove@gmail.com>
2019-10-23 10:28:48 -0400

    While-loop1

---
+++ src/KeepGuessings.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class KeepGuessings {
+    public static void main(String[] args) {
+        System.out.println();
+
+        Scanner keyboard = new Scanner(System.in);
+        int secretNumber, guess = 0;//guess is initialized to 0
+
+        secretNumber = 123;
+
+        System.out.println("I'm thinking of a number between 1 and 1000");
+        System.out.print("Enter the number:");
+        guess = keyboard.nextInt();
+
+        while ( guess != secretNumber )
+        {
+            System.out.println("\nYou are wrong. Try again.");
+            System.out.println("Enter the number: ");
+            guess = keyboard.nextInt();
+        }
+
+        System.out.println("You are correct. You win a prize!");
+        keyboard.close();
+    }
+}
